459252d5a438e58a8b2e8822e097caeeca8510d1
nagimemooo <nagimemooo@gmail.com>
2020-10-04 19:03:46 +0800

    Fix: adopt

--- src/Components/Adopt.js
+++ src/Components/Adopt.js
@@ -5,11 +5,13 @@ import './Adopt.css';
 export default function Adopt() {
   return (
     <div id="top">
-      锚点1
+      {/* 锚点1 <a href="#top"></a>*/}
       <div className="adopt">
         <div className="Item" />
         <h1 className="num">01</h1>
-        <div className="icon"></div>
+        <div className="icon">
+          <i class="fa fa-thumbs-o-up" />
+        </div>
         <div className="txt">照顧</div>
         <div className="txt_desc">當毛小孩隨地大小便</div>
       </div>
