d794055864320d0bd05a81736cefab2cf62219aa
nagimemooo <nagimemooo@gmail.com>
2020-10-09 21:07:12 +0800

    Add: model temp

---
+++ src/Components/CSS/Content.js
@@ -0,0 +1,73 @@
+/** @jsx jsx */ import { css, jsx } from '@emotion/core';
+
+export const ContentStyle = {
+  modalBack: css`
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.8);
+    z-index: 3;
+  `,
+  modal: css`
+    width: 500px;
+    height: 100％;
+    position: fixed;
+    top: 100px;
+    left: 200px;
+    background-color: #fff;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    border-radius: 0.3rem;
+
+    z-index: 3; //蓋掉其他也是浮動的物品
+  `,
+  closeModel: css`
+    border-radius: 5px;
+    border: 3px solid #666;
+    width: 30px;
+    height: 30px;
+    margin: 0 auto;
+    display: block;
+    position: absolute;
+    right: 0px;
+    top: 0px;
+    color: red;
+  `,
+  icon: css`
+    font-size: 60px;
+    color: #ed4c5c;
+    text-align: center;
+  `,
+  txt: css`
+    font-size: 18px;
+    text-align: center;
+  `,
+  txt_desc: css`
+    font-size: 12px;
+    text-align: center;
+    padding: 10px;
+  `,
+  button_more: css`
+    margin: 0 auto;
+    background-color: white;
+    color: #52a8c4;
+    display: block;
+    // 要加這個才能置中 Tip
+    border-radius: 5px;
+    border: 3px solid #666;
+  `,
+  imgCenter: css`
+    display: block;
+    padding: 5px;
+  `,
+  autoBox: css`
+    width: 250px;
+    height: 300px;
+    max-width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  `,
+};
--- src/Components/ContentItem.js
+++ src/Components/ContentItem.js
@@ -1,12 +1,54 @@
 import './Content.css';
 import React, { useState, useEffect } from 'react';
 /** @jsx jsx */ import { css, jsx } from '@emotion/core'; //不知道為什麼一定要加前述
-import { cardStyle } from './CSS/ContentItem';
+import { ContentStyle } from './CSS/Content';
+
+function MyModel(props) {
+  return (
+    <div css={ContentStyle.modalBack}>
+      <div css={ContentStyle.modal}>
+        <button css={ContentStyle.closeModel}>X</button>
+        <div className="">
+          <div>
+            <div className="png">
+              <img
+                src="https://picsum.photos/300/300?image=236"
+                alt=""
+                class="circle-profile"
+              />
+              <div className="realPng">
+                <img src={props.Img} alt="" class="circle-profile" />
+              </div>
+            </div>
+            <div className="desc txt-style">
+              <h3>寵物編號:{props.Id}</h3>
+              <h3>收容位置:{props.Loc}</h3>
+              <h3>年齡:{props.Age === '' ? '未提供' : props.Age}</h3>
+              <h3>性別:{props.Sex}</h3>
+              <h3>寵物編號:{props.Id}</h3>
+              <h3>收容位置:{props.Loc}</h3>
+              <h3>年齡:{props.Age === '' ? '未提供' : props.Age}</h3>
+              <h3>性別:{props.Sex}</h3>
+              <h3>顏色:{props.animal.animal_colour}</h3>
+              <h3>動物體型:{props.animal.animal_bodytype}</h3>
+              <h3>是否絕育:{props.animal.animal_sterilization}</h3>
+              <h3>是否施打狂犬病疫苗:{props.animal.animal_bacterin}</h3>
+              <h3>動物尋獲地:{props.animal.animal_foundplace}</h3>
+              <h3>資料備註:{props.animal.animal_remark}</h3>
+              <h3>其他說明:{props.animal.animal_caption}</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default function Item(props) {
   const [icon, setIcon] = useState('fa fa-heart-o fa-2x');
   const [isClick, setClick] = useState(false);
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
+
   let heartCls = 'fa fa-heart fa-2x';
   let heartOCls = 'fa fa-heart-o fa-2x';
   const getValue = (value) => {
@@ -23,13 +65,10 @@ export default function Item(props) {
   function clickHeart(e) {
     setClick(!isClick);
   }
-  const Flip = () => {
-    setShow(!show);
-    console.log(show);
-  };
+
   return (
-    <div className="item" css={cardStyle.item}>
-      <div css={show ? cardStyle.front : cardStyle.frontClick}>
+    <div className="item">
+      <div>
         <div className="png">
           <img
             src="https://picsum.photos/300/300?image=236"
@@ -52,13 +91,16 @@ export default function Item(props) {
           <h3>收容位置:{props.Loc}</h3>
           <h3>年齡:{props.Age === '' ? '未提供' : props.Age}</h3>
           <h3>性別:{props.Sex}</h3>
-          <button class="button" onClick={Flip}>
+          <button class="button" onClick={() => setShow(true)}>
             點我看更多
           </button>
         </div>
       </div>
-      <div css={show ? cardStyle.back : cardStyle.backClick}>
-        <div className="desc txt-style">
+      <div>
+        {/* <MyModel {...props} /> */}
+        {show ? <MyModel {...props} /> : ''}
+
+        {/* <div className="desc txt-style">
           <h3>寵物編號:{props.Id}</h3>
           <h3>收容位置:{props.Loc}</h3>
           <h3>年齡:{props.Age === '' ? '未提供' : props.Age}</h3>
@@ -71,10 +113,7 @@ export default function Item(props) {
           <h3>動物尋獲地:{props.animal.animal_foundplace}</h3>
           <h3>資料備註:{props.animal.animal_remark}</h3>
           <h3>其他說明:{props.animal.animal_caption}</h3>
-          <button class="button" onClick={Flip}>
-            點我看更多
-          </button>
-        </div>
+        </div>*/}
       </div>
     </div>
   );
